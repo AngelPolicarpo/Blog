7779306c49550eda6f746e1fe45956c33957eb94
AngelPolicarpo <4E6F626F64790A@protonmail.com>
2024-01-13 23:38:55 -0300

    Format document

--- my-app/app/api/auth/[...nextauth]/route.js
+++ my-app/app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,7 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID ,
+      clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     }),
     GoogleProvider({
