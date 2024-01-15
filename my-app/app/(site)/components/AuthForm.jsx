be64279f047843f6012eeedac2d245e6f4119ed9
AngelPolicarpo <4E6F626F64790A@protonmail.com>
2024-01-14 23:48:03 -0300

    Added router and users page

--- my-app/app/(site)/components/AuthForm.jsx
+++ my-app/app/(site)/components/AuthForm.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import axios from "axios";
-import { signIn } from 'next-auth/react';
-import { useCallback, useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { useCallback, useEffect, useState } from 'react';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import { useForm } from 'react-hook-form';
 import { useRouter } from "next/navigation";
@@ -13,9 +13,16 @@ import { toast } from "react-hot-toast";
 
 
 const AuthForm = () => {
+  const session = useSession();
   const router = useRouter();
   const [variant, setVariant] = useState('LOGIN');
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/users')
+    }
+  }, [session?.status, router])
 
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
