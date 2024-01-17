1ef91ac696bb485737112436934497f6fd183cf1
AngelPolicarpo <4E6F626F64790A@protonmail.com>
2024-01-16 23:52:03 -0300

    Added middleware protection

--- my-app/app/(site)/components/AuthForm.jsx
+++ my-app/app/(site)/components/AuthForm.jsx
@@ -51,6 +51,7 @@ const AuthForm = () => {
 
     if (variant === 'REGISTER') {
       axios.post('/api/register', data)
+        .then(() => signIn('credentials', data))
         .catch(() => toast.error('Something went wrong!'))
         .finally(() => setIsLoading(false))
     }
@@ -65,7 +66,7 @@ const AuthForm = () => {
           toast.error('Invalid credentials!')
         }
         if (callback?.ok && !callback?.error) {
-          toast.success('Logged in!')
+          router.push('/users')
         }
       })
       .finally(() => setIsLoading(false))
